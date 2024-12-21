86354aa3eb21a6075dcb0d945f802dbff39fa8dd
Michael <mvetkin@ozon.ru>
2024-12-21 23:36:13 +0300

    Added test on ascii

--- feature/tengwar/ui/teng-processor.test.ts
+++ feature/tengwar/ui/teng-processor.test.ts
@@ -1,10 +1,16 @@
-import { pluginSettings } from 'feature/settings/domain/entity/plugin-settings';
-import { tengProcessor, processCsur } from './teng-processor';
+import { pluginSettings } from '../../settings/domain/entity/plugin-settings';
+import { processCsur, processAscii } from './teng-processor';
 
 describe('CSUR', () => {
 	test('The "Tengwar Formal CSUR" font is used for ConScript Unicode Registry', () => {
 		const innerHTML = processCsur('', pluginSettings());
 
 		expect(innerHTML).toEqual('<div class="tengwar-formal-csur"></div>');
+	});
+
+	test('The "Tengwar Formal CSUR" font is used for ConScript Unicode Registry', () => {
+		const innerHTML = processAscii('2x%51T`Û');
+
+		expect(innerHTML).toEqual('<div class="tengwar-annatar">2x%51T`Û</div>');
 	});
 });
