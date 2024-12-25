382350b02cd8656ee990ff6126b9b0aa93608c20
Michael <mvetkin@ozon.ru>
2024-12-26 01:02:22 +0300

    Fixed defaults

--- feature/settings/domain/entity/default-plugin-settings.ts
+++ feature/settings/domain/entity/default-plugin-settings.ts
@@ -3,7 +3,7 @@ import { pluginSettings } from './plugin-settings';
 export const DEFAULT_PLUGIN_SETTINGS = pluginSettings({
   isHighlightedTehtar: true,
   tehtarColor: '#A78AF9',
-  tengCsurFont: 'Tengwar Telcontar',
-  tengAsciiFont: 'Tengwar Annatar',
+  tengCsurFont: 'TengwarTelcontar',
+  tengAsciiFont: 'TengwarAnnatar',
   tengwarKeywrod: 'teng',
 });
