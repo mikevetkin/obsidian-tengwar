e9509d33fcedc4dd164324f51ac08d14b005fd3d
Michael <mvetkin@ozon.ru>
2024-12-27 01:05:23 +0300

    Added one more font

--- feature/settings/domain/entity/csur-font.ts
+++ feature/settings/domain/entity/csur-font.ts
@@ -1,10 +1,12 @@
 export type CsurFont =
   | 'AlcarinTengwar'
   | 'TengwarAnnatarItalic'
-  | 'TengwarTelcontar';
+  | 'TengwarTelcontar'
+  | 'TengwarTtfTelcontar';
 
 export const CsurFontMap: Record<CsurFont, string> = {
   AlcarinTengwar: 'Alcarin Tengwar',
   TengwarAnnatarItalic: 'Tengwar Artano',
   TengwarTelcontar: 'Tengwar Telcontar',
+  TengwarTtfTelcontar: 'Tengwar Telcontar (TTF)',
 };
