cd5b07316deb48c15fd07fcaec1e5c2139433610
Michael Vetkin <mikevetkin@gmail.com>
2024-12-27 21:41:29 +0300

    Added formal mono font

--- feature/settings/domain/entity/csur-font.ts
+++ feature/settings/domain/entity/csur-font.ts
@@ -2,11 +2,16 @@ export type CsurFont =
   | 'AlcarinTengwar'
   | 'TengwarAnnatarItalic'
   | 'TengwarTelcontar'
-  | 'TengwarTtfTelcontar';
+  // | 'TengwarTtfTelcontar'
+  | 'FreeMonoTengwarTtf';
 
 export const CsurFontMap: Record<CsurFont, string> = {
   AlcarinTengwar: 'Alcarin Tengwar',
   TengwarAnnatarItalic: 'Tengwar Artano',
   TengwarTelcontar: 'Tengwar Telcontar',
-  TengwarTtfTelcontar: 'Tengwar Telcontar (TTF)',
+  /**
+   * Not rendered fine on all platforms
+   */
+  // TengwarTtfTelcontar: 'Tengwar Telcontar (TTF)',
+  FreeMonoTengwarTtf: 'Free Mono Tengwar (TTF)',
 };
