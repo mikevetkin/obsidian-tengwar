f2af344508bc6596e7db382816619a0bbf01decc
Michael <mvetkin@ozon.ru>
2025-01-02 17:32:22 +0300

    Fixed linter

--- feature/settings/domain/entity/default-plugin-settings.ts
+++ feature/settings/domain/entity/default-plugin-settings.ts
@@ -3,7 +3,7 @@ import { pluginSettings } from './plugin-settings';
 export const DEFAULT_PLUGIN_SETTINGS = pluginSettings({
   isHighlightedTehtar: true,
   tehtarColor: '#A78AF9',
-  tengCsurFont: 'TengwarTelcontar',
-  tengAsciiFont: 'TengwarAnnatar',
+  tengCsurFont: 'Telcontar',
+  tengAsciiFont: 'Annatar',
   tengwarKeywrod: 'teng',
 });
