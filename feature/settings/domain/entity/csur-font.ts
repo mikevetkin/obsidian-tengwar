f91234dc4e6a3b442abba3ae1eb66bf51ddafb42
Michael <mvetkin@ozon.ru>
2025-01-02 17:36:40 +0300

    Deleted excess comments

--- feature/settings/domain/entity/csur-font.ts
+++ feature/settings/domain/entity/csur-font.ts
@@ -1,20 +1,8 @@
-export type CsurFont =
-  | 'Alcarin'
-  | 'Artano'
-  | 'Telcontar'
-  /**
-   * TODO: Delete from css
-   */
-  // | 'TengwarTtfTelcontar'
-  | 'FreeMono';
+export type CsurFont = 'Alcarin' | 'Artano' | 'Telcontar' | 'FreeMono';
 
 export const CsurFontMap: Record<CsurFont, string> = {
   Alcarin: 'Alcarin',
   Artano: 'Artano (italic)',
   Telcontar: 'Telcontar',
-  /**
-   * Not rendered fine on all platforms
-   */
-  // TengwarTtfTelcontar: 'Tengwar Telcontar (TTF)',
   FreeMono: 'Free Mono',
 };
