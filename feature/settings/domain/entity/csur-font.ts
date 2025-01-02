9ea6f1cea082de5f01d38bc54dfe27e0c8dd3393
Michael <mvetkin@ozon.ru>
2025-01-02 17:11:20 +0300

    Added italic label

--- feature/settings/domain/entity/csur-font.ts
+++ feature/settings/domain/entity/csur-font.ts
@@ -2,16 +2,19 @@ export type CsurFont =
   | 'AlcarinTengwar'
   | 'TengwarAnnatarItalic'
   | 'TengwarTelcontar'
+  /**
+   * TODO: Delete from css
+   */
   // | 'TengwarTtfTelcontar'
   | 'FreeMonoTengwarTtf';
 
 export const CsurFontMap: Record<CsurFont, string> = {
   AlcarinTengwar: 'Alcarin Tengwar',
-  TengwarAnnatarItalic: 'Tengwar Artano',
+  TengwarAnnatarItalic: 'Tengwar Artano (italic)',
   TengwarTelcontar: 'Tengwar Telcontar',
   /**
    * Not rendered fine on all platforms
    */
   // TengwarTtfTelcontar: 'Tengwar Telcontar (TTF)',
-  FreeMonoTengwarTtf: 'Free Mono Tengwar (TTF)',
+  FreeMonoTengwarTtf: 'Free Mono Tengwar',
 };
